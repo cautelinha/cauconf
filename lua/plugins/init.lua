a71292d684609aff0628354a23114fd16b0f44a8
cautelinha <yuri.ss.dev@gmail.com>
2022-03-29 11:12:13 -0300

    feat: added status line

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,6 @@
 return require'packer'.startup(function()
 	use 'wbthomason/packer.nvim'
+	use 'kyazdani42/nvim-web-devicons'
 	use 'https://github.com/jiangmiao/auto-pairs.git'
 	use 'https://github.com/tpope/vim-fugitive.git'
 	use 'https://github.com/tomasr/molokai.git'
@@ -7,5 +8,11 @@ return require'packer'.startup(function()
 	use {
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate'
+	}
+
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+		config = require('plugins/configs/lualine')
 	}
 end)
